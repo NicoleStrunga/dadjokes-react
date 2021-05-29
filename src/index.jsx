6a5c63d42d987fd7a164d757c56bf1031dc7dcf7
NicoleStrunga <nicole.strungova@gmail.com>
2021-05-29 15:22:42 +0200

    Vytvoření komponenty Joke, napojeni jokes.js, seznam - 15.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
+import { Joke } from './Joke';
+import { jokes } from './jokes';
 import './style.css';
 
 const App = () => {
@@ -55,6 +57,16 @@ const App = () => {
           </span>
         </div>
       </div>
+      {jokes.map((joke) => (
+        <Joke
+          key={joke.id}
+          userAvatar={joke.avatar}
+          userName={joke.name}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      ))}
     </div>
   );
 };
